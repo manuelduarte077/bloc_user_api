511111f45405791e0b51a97d27fe9343e1f6d8b3
@manuelduarte077 <carlosd99m@gmail.com>
2022-06-26 20:56:50 -0600

    refactor screens user details

--- lib/features/user/presentation/screens/user_detail_screens.dart
+++ lib/features/user/presentation/screens/user_detail_screens.dart
@@ -21,13 +21,117 @@ class UserDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(user.name),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite_outline),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Text(user.email),
-            Text(user.phone),
-            Text(user.website),
+            Image.network(
+              'https://i.pinimg.com/736x/5a/3f/01/5a3f0150b9031f6642ffdf39984bb945.jpg',
+              height: 250,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const Text(
+                    'Name',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    user.name,
+                  ),
+                  const Text(
+                    'Email',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    user.email,
+                  ),
+                  const SizedBox(height: 6),
+                  const Text(
+                    'Phone',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    user.phone,
+                  ),
+                  const SizedBox(height: 6),
+                  const Text(
+                    'Website',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    user.website,
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  const Text(
+                    'Company',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    user.company!.name,
+                  ),
+                  const SizedBox(height: 6),
+                  const Text(
+                    'Address',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    user.address!.street,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
